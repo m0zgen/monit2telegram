cfabbe476428b56b59ffcf67e57b3240b5ba9fb3
root <root@debian-bld-local>
2022-07-10 19:18:46 +0600

    Server IP in message

--- monit2telegram.sh
+++ monit2telegram.sh
@@ -4,4 +4,7 @@
 #
 # Depends on having `/usr/local/bin/sendtelegram` installed and a config file in `/etc/telegramrc`
 #
-/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
+
+SERVER_IP=`hostname -I`
+
+/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION. Host IP(s): $SERVER_IP"
