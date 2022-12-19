162a00d890d65d270de1201013bc3f80e776248e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-12-19 17:07:01 +0600

    -m

--- monit2telegram.sh
+++ monit2telegram.sh
@@ -6,5 +6,10 @@
 #
 
 SERVER_IP=`hostname -I`
+MESSAGE_DATA=$1
 
-/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION. Host IP(s): $SERVER_IP"
+if [ -z "${MESSAGE_DATA}" ]; then
+    MESSAGE_DATA="None"
+fi
+
+/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION. Host IP(s): $SERVER_IP. Message: $MESSAGE_DATA"
